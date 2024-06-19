702280fe4782027b87fc1da2bdcae17aa50e92a2
Victor <lessricc@gmail.com>
2024-06-19 19:01:41 +0200

    hice el primero

--- src/js/index.js
+++ src/js/index.js
@@ -1,2 +1,9 @@
 // El styles lo importamos aquí para que se encargue Vite de compilar todo
 import '../scss/styles.scss';
+//- Crea una función que reciba un array de números y muestre por consola cada número multiplicado por su índice en el array
+
+const numbers = [1, 2, 3, 4, 5];
+
+numbers.forEach((number, index) => {
+  console.log(number * index);
+});
